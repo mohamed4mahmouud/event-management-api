b58d682e78034d5e1bc86d8e4aab438b131d7f6c
mohamed4mahmouud <mohamedalamrawy55@gmail.com>
2024-05-01 02:27:43 +0300

    revoke token and logout

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::prefix('v1')->group(function(){
     Route::apiResource('/events', EventController::class);
     Route::apiResource('events.attendees', AttendeeController::class)->scoped()->except('update');
     Route::post('/login', [AuthController::class ,'login']);
+    Route::post('/logout' , [AuthController::class , 'logout'])->middleware('auth:sanctum');
 });
 
 Route::get('/user', function (Request $request) {
